510552b3a742dcf4268ce36621eb0e6002209a9d
donzella99 <46731463+donzella99@users.noreply.github.com>
2021-06-10 00:05:31 -0700

    Changes

--- hw6_python.py
+++ hw6_python.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import  jsonify,render_template,request
+from flask import  jsonify,render_template,request, redirect
 import requests
 
 
@@ -11,11 +11,34 @@ class DataStore():
     keyword = None
     geopoint = None
     radius = None
+    location = None
+    category = None
 data = DataStore()
 
+@app.route('/getlat')
+def geolocation_func(val):
+     val = val.split(" ")
+     print(val)
+     add_on = "";
+     for y in val:
+         add_on += "+";
+         add_on += y
+     url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAjU6cgR6QnwPyH5ICJAbcl7_D4NAxwJ2c&address=" + add_on
+     print(url)
+     x = geo_extraction(url)
+     comma = ","
+     if(data.location == None):
+         print("KOIKLKKH")
+         data.location = (str(x['results'][0]['geometry']['location']['lat']) + comma + str(x['results'][0]['geometry']['location']['lng']))
+         print(data.location)
+     return 1
+
+def geo_extraction(url):
+    req = requests.get(url)
+    json_request = req.json()
+    return json_request
 
 
-global url
 @app.route('/')
 def hello():
     return app.send_static_file('index.html')
@@ -35,29 +58,45 @@ def event_extraction(url):
     #print(json_request)
     return json_obj
 
-@app.route('/events')
-def big():
-    #url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=qcOhKgPQynndjc1pcDNq0flHYCg2ltMF"+"&keyword=" + data.keyword + "&geoPoint="+ data.geopoint + "&radius=" + data.radius
-    url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=qcOhKgPQynndjc1pcDNq0flHYCg2ltMF"
-    print(url)
-    x = event_extraction(url)
-    data.keyword = None
-    data.geopoint = None
-    data.radius = None
-    return x
 
+@app.route('/test', methods=['GET','POST'])
+def event():
 
-@app.route('/test', methods=['POST','GET'])
-def events():
     event_data = request.json
     print(event_data)
     data.keyword = event_data['keyword']
     data.geopoint = event_data['geoPoint']
     data.radius = event_data['radius']
-    print(data.geopoint)
+    data.category = event_data['category']
+    temp = event_data['location']
+    geolocation_func(temp)
 
-    return "success"
+    return "true"
 
+@app.route('/events')
+def big():
+    print("Happened")
+    try:
+        print(data.location)
+        print("Hello")
+        x = ""
+        print(data.location)
+        url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=qcOhKgPQynndjc1pcDNq0flHYCg2ltMF"+"&keyword=" + data.keyword + "&radius=" + data.radius + "&geoPoint=" + data.location + "&SegmentId=" + data.category
+        #url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=qcOhKgPQynndjc1pcDNq0flHYCg2ltMF"
+        print(url)
+        x = event_extraction(url)
+        data.keyword = None
+        data.geopoint = None
+        data.radius = None
+        data.location = None
+        data.category = None
+        return x
+    except:
+        if(data.location == None):
+            redirect(render_template('/test'))
+        return("not_now")
+
+    #url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=qcOhKgPQynndjc1pcDNq0flHYCg2ltMF"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/index1.js
+++ static/js/index1.js
@@ -1,18 +1,68 @@
 /*jshint esversion: 8 */
-var obj;
-var search_obj;
-var use_obj;
+var obj = "";
+var use_obj = "";
+var search_obj = "";
 var ticketmaster;
 var curr;
 var button_1;
 var button_2;
 
+document.getElementById("search").addEventListener("click",function (){
+    if(document.getElementById("keyword").value.length == 0){
+        document.getElementById("msg").innerHTML = " Please fill out this field.";
+    }
+    else if(document.getElementById("category").value.length == 0){
+        document.getElementById("msg").innerHTML = "your message";
+    }
+    else{
+        if(document.getElementById("location").value != "location" && document.getElementById("location").value != ""){
+                console.log("c1");
+                console.log(search_obj);
+        }
+        document.getElementById("msg").innerHTML = "";
+        receive_events();
+        receive_events();
+        receive_events();
+        console.log("JALAKSLA");
+        console.log(search_obj);
+        if(button_1 == 1){
+            use_obj = obj;
+        }
+        else{
+            use_obj = search_obj;
+        }
+
+        send_keyword();
+        send_keyword();
+        send_keyword();
+    }
+});
+
+document.getElementById("clear").addEventListener("click",function (){
+    if(document.getElementById("keyword").value.length != 0){
+        document.getElementById("keyword").value = "";
+        delete_ext();
+        delete_row(ticketmaster);
+    }
+    if(document.getElementById("location").value.length != 0){
+        document.getElementById("location").value = "";
+    }
+});
+
+document.getElementById("here_button").addEventListener("click",function (){
+    button1();
+});
+
+document.getElementById("checkbox_location").addEventListener("click",function (){
+    button2();
+});
+
 function init(){
      document.getElementById("keyword").disabled = true;
+     document.getElementById("location").disabled = true;
      button_1 = 1;
      button_2 = 0;
      document.getElementById("here_button").checked= true;
-     document.getElementById("event-table").style.display = "none";
 }
 
 function getdata(){
@@ -24,6 +74,7 @@ function getdata(){
     .then(data => {
          //   console.log(data);
             obj = data.loc;
+            console.log("LOL");
             console.log(obj);
             //document.getElementById("here-location").disabled = false;
 
@@ -51,23 +102,28 @@ function find_location(){
         })
 
         .then(data => {
-            search_obj = data.results[0].geometry.bounds.northeast.lat +  data.results[0].geometry.bounds.northeast.lng;
-            console.log(search_obj);
-            document.getElementById("keyword").disabled = false;
+            if(typeof data != 'undefined'){
+                search_obj = data.results[0].geometry.bounds.northeast.lat+ "," + data.results[0].geometry.bounds.northeast.lng;
+                console.log(typeof search_obj);
+                console.log(search_obj);
+                document.getElementById("keyword").disabled = false;
+                return 1;
+            }
         });
-    return 1;
 }
-
-
-
 
 function receive_events(){
     fetch("/events").then(function(value) {
-    return value.json();
+        if(value.ok){
+            return value.json();
+        }
+        else{
+            return receive_events();
+        }
     })
     .then(
         (value) => {
-        //    console.log(value);
+            console.log(value);
             print_events(value);
             ticketmaster = value;
         }
@@ -77,6 +133,11 @@ function receive_events(){
 function extend_event(){
     events_ticketmaster = ticketmaster;
     var table1 = document.getElementById("expand-table");
+    var size = (events_ticketmaster.page.totalElements) * 215;
+    console.log(size);
+    document.getElementById("expand-table").style.paddingTop = 'size'+'px';
+    console.log("LENGTH");
+    console.log(curr);
     var row = table1.insertRow(0);
     var cell1 = row.insertCell(0);
     cell1.innerHTML = '<h1>' + events_ticketmaster._embedded.events[curr].name + '</h1>';
@@ -114,9 +175,26 @@ function extend_event(){
     cell3.appendChild(img);
 }
 
+function delete_ext(){
+    var y = (document.getElementById("expand-table").rows.length);
+        for(x = 0; x<y; x++){
+            document.getElementById("expand-table").deleteRow(0);
+        }
+}
+
 function add_row(events_ticketmaster){
     document.getElementById("event-table").style.display = "block";
-    for(i = 1; i<=events_ticketmaster.page.totalElements; i++){
+    document.getElementById("head1").style.color = "black";
+    document.getElementById("head2").style.color = "black";
+    document.getElementById("head3").style.color = "black";
+    document.getElementById("head4").style.color = "black";
+    document.getElementById("head5").style.color = "black";
+    var max = 20;
+    if(events_ticketmaster.page.totalElements<20){
+        max = events_ticketmaster.page.totalElements;
+    }
+
+    for(i = 1; i<=max; i++){
         curr = i-1;
         var table1 = document.getElementById("event-table");
         var row = table1.insertRow(i);
@@ -134,32 +212,37 @@ function add_row(events_ticketmaster){
         cell2.style.border = "solid #000000";
         cell3.style.border = "solid #000000";
         cell4.style.border = "solid #000000";
-        var date = events_ticketmaster._embedded.events[i-1].dates.start.localDate + " " + events_ticketmaster._embedded.events[i-1].dates.start.localTime;
+        var date = events_ticketmaster._embedded.events[curr].dates.start.localDate + " " + events_ticketmaster._embedded.events[curr].dates.start.localTime;
 
         cell0.innerHTML = date;
         var img = document.createElement('img');
-        var images = events_ticketmaster._embedded.events[i-1].images;
+        var images = events_ticketmaster._embedded.events[curr].images;
         for(z =0; z<images.length; z++){
-            if(events_ticketmaster._embedded.events[i-1].images[z].width == 305){
-                img.src = events_ticketmaster._embedded.events[i-1].images[z].url;
+            if(events_ticketmaster._embedded.events[curr].images[z].width == 305){
+                img.src = events_ticketmaster._embedded.events[curr].images[z].url;
             }
         }
 
 
         cell1.appendChild(img);
-        cell2.innerHTML ='<a href="#" onclick=extend_event(); return false;>'+events_ticketmaster._embedded.events[i-1].name +'<\a>';
-        cell3.innerHTML = events_ticketmaster._embedded.events[i-1].classifications[0].segment.name;
-        cell4.innerHTML = events_ticketmaster._embedded.events[i-1]._embedded.venues[0].name;
+        cell2.innerHTML ='<a href="#" onclick=extend_event(); return false;>'+events_ticketmaster._embedded.events[curr].name +'<\a>';
+        cell3.innerHTML = events_ticketmaster._embedded.events[curr].classifications[0].segment.name;
+        cell4.innerHTML = events_ticketmaster._embedded.events[curr]._embedded.venues[0].name;
+        document.getElementById("event-table").style.border = "solid #000000";
     }
-    document.getElementById("event-table").style.border = "solid #000000";
 }
 
 function delete_row(events_ticketmaster){
     var y = (document.getElementById("event-table").rows.length);
+    document.getElementById("head1").style.color = "white";
+    document.getElementById("head2").style.color = "white";
+    document.getElementById("head3").style.color = "white";
+    document.getElementById("head4").style.color = "white";
+    document.getElementById("head5").style.color = "white";
     for(x = 1; x<y; x++){
-    //    console.log(x);
         document.getElementById("event-table").deleteRow(1);
     }
+    document.getElementById("event-table").style.border = "solid #FFFFFF";
 }
 
 
@@ -170,15 +253,16 @@ function print_events(events_ticketmaster){
     if(events_ticketmaster.page.totalElements != 0 && document.getElementById("event-table").rows.length==2 ){
         add_row(events_ticketmaster);
         document.getElementById("no-events").innerHTML  = "";
-        //extend_event(events_ticketmaster);
     }
     else if((document.getElementById("event-table").rows.length)!=2 && events_ticketmaster.page.totalElements != 0){
             delete_row(events_ticketmaster);
+            delete_ext();
             add_row(events_ticketmaster);
             document.getElementById("no-events").innerHTML  = "";
     }
     else{
         delete_row(events_ticketmaster);
+        delete_ext();
         document.getElementById("no-events").innerHTML  = "No Events Found! Please Enter Another Search Keyword";
     }
 }
@@ -186,7 +270,6 @@ function print_events(events_ticketmaster){
 
 function button_off(){
     document.addEventListener("DOMContentLoaded", function(event) {
-
     });
 }
 
@@ -196,23 +279,44 @@ function keyword_result() {
 }
 
 function button1(){
-    console.log("1");
     button_1 = 1;
     button_2 = 0;
+    document.getElementById("location").disabled = true;
 }
 function button2(){
-    console.log("2");
     button_1 = 0;
     button_2 = 1;
+    document.getElementById("location").disabled = false;
 }
 
-function send_keyword() {
+function send_keyword() { //SEND THE JSON STRING WITH ALL THE VALUES FOR TICKETMASTER
     var radius = "";
     if(document.getElementById("radius").value == ""){
         radius = String(document.getElementById("radius").placeholder);
     }
     else{
         radius = document.getElementById("radius").value;
+        console.log(radius);
+    }
+    var content_val = (document.getElementById("category").value);
+    var category_selected;
+    if(content_val == 1){
+        category_selected = "";
+    }
+    else if(content_val == 2){
+        category_selected = "KZFzniwnSyZfZ7v7nJ";
+    }
+    else if(content_val == 3){
+        category_selected = "KZFzniwnSyZfZ7v7nE";
+    }
+    else if(content_val == 4){
+        category_selected = "KZFzniwnSyZfZ7v7na";
+    }
+    else if(content_val == 5){
+        category_selected = "KZFzniwnSyZfZ7v7nn";
+    }
+    else if(content_val == 6){
+        category_selected = "KZFzniwnSyZfZ7v7n1";
     }
     (async () => {
         const rawResponse = await fetch('/test', {
@@ -221,7 +325,7 @@ function send_keyword() {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
         },
-        body: JSON.stringify({"keyword": document.getElementById("keyword").value,"geoPoint":use_obj,"radius":radius})
+        body: JSON.stringify({"keyword": document.getElementById("keyword").value,"geoPoint":obj,"radius":radius, "location": document.getElementById("location").value, "category": category_selected})
     });
     const content = await rawResponse.json();
 
@@ -232,71 +336,3 @@ function send_keyword() {
 init();
 button_off();
 getdata();
-
-document.getElementById("search").addEventListener("click",function (){
-    if(document.getElementById("keyword").value.length == 0){
-        document.getElementById("msg").innerHTML = " Please fill out this field.";
-    }
-    else if(document.getElementById("category").value.length == 0){
-        document.getElementById("msg").innerHTML = "your message";
-    }
-    else{
-        document.getElementById("msg").innerHTML = "";
-        receive_events();
-        find_location();
-        if(button_1 == 1){
-            use_obj = obj;
-        }
-        else{
-            use_obj = search_obj;
-        }
-        send_keyword();
-    }
-});
-
-document.getElementById("clear").addEventListener("click",function (){
-    if(document.getElementById("keyword").value.length != 0){
-        document.getElementById("keyword").value = "";
-        delete_row(ticketmaster);
-    }
-    if(document.getElementById("location").value.length != 0){
-        document.getElementById("location").value = "";
-    }
-});
-
-document.getElementById("here_button").addEventListener("click",function (){
-    button1();
-});
-
-document.getElementById("checkbox_location").addEventListener("click",function (){
-    button2();
-    console.log(use_obj);
-});
-
-
-console.log(obj);
-
-
-
-// document.getElementById("here_button").addEventListener("click",function(){
-//     console.log("5");
-//     document.getElementById("here_button").click();
-//     if(document.getElementById("here_button").click()){
-//         console.log("1");
-//         if(document.getElementById("checkbox_location").click()){
-//             console.log("2");
-//             document.getElementById("checkbox_location").click();
-//             console.log("3");
-//         }
-//     }
-// });
-
-// function checkbox1(){
-//     document.getElementById("here_button").click();
-//
-// }
-//
-// function checkbox2(){
-//     document.getElementById("checkbox_location").click();
-//     //document.getElementById("here_button").disabled = true;
-// }
